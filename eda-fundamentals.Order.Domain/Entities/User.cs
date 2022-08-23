b80a385f08652687e5afccb6007a7a01a517928e
Fernando Fujikawa <ferfujikawa@gmail.com>
2022-08-23 12:24:33 -0300

    Criação da entidade User.

---
+++ eda-fundamentals.Order.Domain/Entities/User.cs
@@ -0,0 +1,14 @@
+﻿using eda_fundamentals.Shared.Entities;
+
+namespace eda_fundamentals.Order.Domain.Entities
+{
+    public class User : Entity
+    {
+        public User(string email) : base()
+        {
+            Email = email;
+        }
+
+        public string Email { get; private set; }
+    }
+}
