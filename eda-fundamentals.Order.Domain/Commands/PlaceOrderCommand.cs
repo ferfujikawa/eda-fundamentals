2e46f4c0fc5df4e2f0ca0d5b20c3f76254970573
Fernando Fujikawa <ferfujikawa@gmail.com>
2022-08-23 13:33:13 -0300

    Criação de PlaceOrderCommand e PlaceOrderHandler.

---
+++ eda-fundamentals.Order.Domain/Commands/PlaceOrderCommand.cs
@@ -0,0 +1,21 @@
+﻿using eda_fundamentals.Shared.Commands;
+
+namespace eda_fundamentals.Order.Domain.Commands
+{
+    public class PlaceOrderCommand : ICommand
+    {
+        public PlaceOrderCommand()
+        {
+            Items = new List<OrderItemCommand>();
+        }
+
+        public Guid User { get; set; }
+        public IList<OrderItemCommand> Items { get; set; }
+    }
+
+    public class OrderItemCommand
+    {
+        public Guid Product { get; set; }
+        public decimal Quantity { get; set; }
+    }
+}
---
+++ eda-fundamentals.Order.Domain/Handler/PlaceOrderHandler.cs
@@ -0,0 +1,44 @@
+﻿using eda_fundamentals.Order.Domain.Commands;
+using eda_fundamentals.Order.Domain.Repositories;
+using eda_fundamentals.Shared.Commands.Handlers;
+
+namespace eda_fundamentals.Order.Domain.Handler
+{
+    public class PlaceOrderHandler : ICommandHandler<PlaceOrderCommand>
+    {
+        private readonly IOrderRepository _orderRepository;
+        private readonly IUserRepository _userRepository;
+        private readonly IProductRepository _productRepository;
+        public PlaceOrderHandler(
+            IOrderRepository orderRepository,
+            IUserRepository userRepository,
+            IProductRepository productRepository)
+        {
+            _orderRepository = orderRepository;
+            _userRepository = userRepository;
+            _productRepository = productRepository;
+        }
+
+        public async Task HandleAsync(PlaceOrderCommand command)
+        {
+            #region Criação das entidades
+            var user = await _userRepository.GetByIdAsync(command.User);
+            if (user == null)
+                throw new Exception("Usuário não encontrado");
+
+            var order = new Entities.Order(user);
+
+            command.Items.ToList().ForEach(async x =>
+                {
+                    var product = await _productRepository.GetByIdAsync(x.Product);
+                    if (product == null)
+                        throw new Exception("Produto não encontrado");
+
+                    order.AddItem(product, x.Quantity);
+                });
+            #endregion
+
+            await _orderRepository.CreateAsync(order);
+        }
+    }
+}
