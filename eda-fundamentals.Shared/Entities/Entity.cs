f74debbbf44aebf28adc8fe446be6a354890045e
Fernando Fujikawa <ferfujikawa@gmail.com>
2022-08-23 12:14:53 -0300

    Criação da classe abstrata Entity.

---
+++ eda-fundamentals.Shared/Entities/Entity.cs
@@ -0,0 +1,12 @@
+﻿namespace eda_fundamentals.Shared.Entities
+{
+    public abstract class Entity
+    {
+        protected Entity()
+        {
+            Id = Guid.NewGuid();
+        }
+
+        public Guid Id { get; private set; }
+    }
+}
