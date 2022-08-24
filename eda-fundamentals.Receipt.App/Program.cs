4873fcff61f3357fc88a0fc746b62559f4e5172b
Fernando Fujikawa <ferfujikawa@gmail.com>
2022-08-24 13:38:03 -0300

    Implementação de carregamento de appsettings.json. Implementação da inicialização de Receipt.App para injeção de dependências.

--- eda-fundamentals.Receipt.App/Program.cs
+++ eda-fundamentals.Receipt.App/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace eda_fundamentals.Receipt.App;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetParent(AppContext.BaseDirectory)?.FullName)
+            .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+
+        var configuration = builder.Build();
+
+        var serviceCollection = new ServiceCollection();
+        serviceCollection.AddServices();
+
+        var serviceProvider = serviceCollection.BuildServiceProvider();
+        Console.WriteLine(configuration.GetValue<string>("mensagem"));
+    }
+}
